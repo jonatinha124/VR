ec9552e0db422dc32d74555da5925beaec9197a5
jonatinha124 <jonatasras@gmail.com>
2023-03-01 15:06:15 -0300

    projectile movement

---
+++ Assets/script/PlayerAction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    public GameObject bullet;
+    public float speed = 10f;
+
+    void Start(){
+
+        Debug.Log("começou o jogo");
+
+
+    }
+
+    void Update(){
+        if (Input.GetButtonDown("Fire1")){
+          GameObject createdbullet = Instantiate(bullet);
+            createdbullet.transform.position = transform.position;
+
+            Rigidbody bullet_rig = createdbullet.GetComponent<Rigidbody>();
+            Camera cam = GetComponentInChildren<Camera>();
+
+            bullet_rig.velocity = cam.transform.rotation * Vector3.forward * speed;
+        }
+    }
+}
