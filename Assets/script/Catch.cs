1c4fa62d454e435c6d65e32e1a72eb5e469cc459
jonatinha124 <jonatasras@gmail.com>
2023-03-07 15:32:20 -0300

    contador

---
+++ Assets/script/Catch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catch : MonoBehaviour
+{
+   Counter contadorDeInimigos;
+
+   void Start()
+    {
+        contadorDeInimigos = FindObjectOfType<Counter>();
+    }
+   void OnCollisionEnter(Collision objeto)
+    {
+        if(objeto.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Bateu!");
+            contadorDeInimigos.Increment();
+        }
+    }
+}
---
+++ Assets/script/Counter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Counter : MonoBehaviour
+{
+    public int counter;
+    public TMP_Text counter_text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 0;
+    }
+
+   public void Increment()
+    {
+        counter += 1;
+        counter_text.text = "Enemies" + counter; 
+        Debug.Log("Counter: " + counter);
+    }
+}
